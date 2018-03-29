48d15b382450b5ca8cbf4b7c8fb8a0fb5f33cd2b
Maryna Delidova <marynadelidova@marynas-macbook-pro.local>
2018-03-29 09:59:39 +0300

    Initial Commit

---
+++ DictionaryTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DictionaryTest
+//
+//  Created by Maryna Delidova on 3/29/18.
+//  Copyright © 2018 Maryna Delidova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
